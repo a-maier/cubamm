4bbe64f86a497d84f3244b273d62f61771d4b870
Andreas Maier <andreas.maier@durham.ac.uk>
2017-07-12 09:34:50 +0100

    support for complex return types

--- include/cubamm/integrators.hpp
+++ include/cubamm/integrators.hpp
@@ -2,6 +2,7 @@
 
 #include <limits>
 #include <memory>
+#include <complex>
 
 #include "cubamm/algorithms.hpp"
 #include "cubamm/range.hpp"
@@ -127,8 +128,31 @@ namespace cubamm{
     }
 
     inline
-    void copy_to_double_arr(double in, double to[]){
-      to[0] = in;
+    void copy_to_double_arr(double from, double to[]){
+      to[0] = from;
+    }
+
+    inline
+    void copy_to_double_arr(std::complex<double> const & from, double to[]){
+      to[0] = real(from);
+      to[1] = imag(from);
+    }
+
+    template<size_t N>
+    void copy_to_double_arr(std::array<double, N> const & from, double to[]){
+      std::copy(begin(from), end(from), to);
+    }
+
+    template<size_t N>
+    void copy_to_double_arr(
+        std::array<std::complex<double>, N> const & from,
+        double to[]
+    ){
+      size_t i = 0;
+      for(std::complex<double> const & entry: from){
+        to[i++] = real(entry);
+        to[i++] = imag(entry);
+      }
     }
 
     template<typename Result, typename... T>
@@ -252,6 +276,56 @@ namespace cubamm{
           &res.nregions, &res.neval, &res.fail,
           res.integral.data(), res.error.data(), res.prob.data()
       );
+      return res;
+    }
+
+    template<typename F, typename... Number>
+    auto integrate(
+        std::complex<double> /* function f return type */,
+        F f, Range<Number>... r
+    ){
+      auto data = detail::make_IntegrandData(std::move(f), std::make_tuple(r...));
+      Result<std::complex<double>> res;
+      std::array<double, 2> integral, error, prob;
+      ::Cuhre(
+          sizeof...(r), integral.size(),
+          detail::as_cuba_integrand<decltype(data)>, &data, nvec(),
+          epsrel(), epsabs(),
+          flags(),
+          mineval(), maxeval(),
+          key(), statefile(), spin(),
+          &res.nregions, &res.neval, &res.fail,
+          integral.data(), error.data(), prob.data()
+      );
+      res.integral = std::complex<double>{integral[0], integral[1]};
+      res.error = std::complex<double>{error[0], error[1]};
+      res.prob = std::complex<double>{prob[0], prob[1]};
+      return res;
+    }
+
+    template<size_t N, typename F, typename... Number>
+    auto integrate(
+        std::array<std::complex<double>, N> /* function f return type */,
+        F f, Range<Number>... r
+    ){
+      auto data = detail::make_IntegrandData(std::move(f), std::make_tuple(r...));
+      Result<std::array<std::complex<double>, N>> res;
+      std::array<double, 2*N> integral, error, prob;
+      ::Cuhre(
+          sizeof...(r), integral.size(),
+          detail::as_cuba_integrand<decltype(data)>, &data, nvec(),
+          epsrel(), epsabs(),
+          flags(),
+          mineval(), maxeval(),
+          key(), statefile(), spin(),
+          &res.nregions, &res.neval, &res.fail,
+          integral.data(), error.data(), prob.data()
+      );
+      for(size_t i = 0; i < res.integral.size(); ++i){
+        res.integral[i] = std::complex<double>{integral[2*i], integral[2*i+1]};
+        res.error[i] = std::complex<double>{error[2*i], error[2*i+1]};
+        res.prob[i] = std::complex<double>{prob[2*i], prob[2*i+1]};
+      }
       return res;
     }
 
