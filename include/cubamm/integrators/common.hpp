018df1c27ccb46e3ce1bcfe65fd4c60a1ce99339
Andreas Maier <andreas.maier@durham.ac.uk>
2017-07-19 11:44:49 +0100

    added license notes

--- include/cubamm/integrators/common.hpp
+++ include/cubamm/integrators/common.hpp
@@ -1,3 +1,34 @@
+/**
+ * @file
+ * @brief Common integrator utilities
+ *
+ * This header defines utility functions and classes shared by
+ * the various integrators.
+ *
+ * @author  Andreas Maier <andreas.maier@durham.ac.uk>
+ * @version 0.0.0
+ *
+ * @section LICENSE
+ *
+ * Copyright 2017 Andreas Maier
+ *
+ * This file is part of the cubamm library.
+ *
+ * The cubamm library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * The cubamm library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the cubamm library. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ */
 #pragma once
 
 #include <limits>
